02da6ebb52927bc8922f98122d8c4a64f1c3e358
Andreea <andelenasamur@gmail.com>
2023-08-31 12:15:05 +0200

    create auth routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,23 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { PagesRoutingModule } from './pages/pages.routing';
-
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+import { AuthRoutingModule } from './auth/auth.routing';
 
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-
   { path: '**', component: NopagefoundComponent },
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
-    PagesRoutingModule
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [
     RouterModule
