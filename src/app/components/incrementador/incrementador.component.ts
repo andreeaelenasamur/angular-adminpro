51baeef9b86c8eac46a8a2a998408ac40bc9e28a
Andreea <andelenasamur@gmail.com>
2023-09-01 10:15:32 +0200

    conditional button color

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -6,29 +6,33 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class IncrementadorComponent {
+export class IncrementadorComponent implements OnInit{
 
-  progreso: number = 40;
-
-  get getPorcentaje() {
-    return `${this.progreso}%`;
+  ngOnInit() {
+    this.btnClass= `btn ${this.btnClass}`;
   }
+
+  @Input('valor') progreso: number = 40;
+  @Input() btnClass: string = 'btn-primary';
+
+  @Output() valorSalida: EventEmitter<number> = new EventEmitter();
 
   cambiarValor( valor: number ) {
 
     if( this.progreso >= 100 && valor >= 0 ){
+      this.valorSalida.emit(100);
       this.progreso = 100;
       return;
     }
 
 
     if( this.progreso <= 0 && valor < 0 ){
-      this.progreso = 0;
+      this.valorSalida.emit(0);
       return;
     }
 
     this.progreso = this.progreso + valor;
-
+    this.valorSalida.emit(this.progreso);
   }
 
 }
