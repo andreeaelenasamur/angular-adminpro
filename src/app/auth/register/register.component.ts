579d3f685a4528e9b35bbf2c4eb4ae1edaf6a16c
Andreea <andelenasamur@gmail.com>
2023-09-12 10:28:56 +0200

    validate equal passwords

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -13,9 +13,11 @@ export class RegisterComponent {
   public registerForm: any = this.fb.group({
     nombre: ['Fernando', Validators.required ],
     email: ['test100@gmail.com', [Validators.required, Validators.email] ],
-    password: ['', Validators.required ],
-    password2: ['', Validators.required ],
-    terminos: [false, Validators.required ],
+    password: ['123456', Validators.required ],
+    password2: ['1234567', Validators.required ],
+    terminos: [true, Validators.required ],
+  }, {
+    validators: this.passwordsIguales('password', 'password2')
   });
 
   constructor( private fb: FormBuilder) {}
@@ -41,10 +43,39 @@ export class RegisterComponent {
 
   }
 
+  contrasenasNoValidas() {
+    const pass1 = this.registerForm.get('password').value;
+    const pass2 = this.registerForm.get('password2').value;
+
+    if( (pass1 !== pass2) && this.formSubmitted ) {
+      return true;
+    } else {
+      return false;
+    }
+
+
+  }
+
   aceptaTerminos() {
     return !this.registerForm.get('terminos').value && this.formSubmitted;
   }
 
+  passwordsIguales( pass1Name: string, pass2Name: string ) {
 
+    return ( formGroup: FormGroup ) => {
+
+      const pass1Control: any = formGroup.get(pass1Name);
+      const pass2Control: any = formGroup.get(pass2Name);
+
+      if( pass1Control.value === pass2Control.value ) {
+        pass2Control.setErrors(null)
+      } else {
+        pass2Control.setErrors({ noEsIgual: true })
+      }
+
+    }
+
+
+  }
 
 }
