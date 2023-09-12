6c6b596db6109243f92af82a17b9c4a141bd7e40
Andreea <andelenasamur@gmail.com>
2023-09-12 10:13:29 +0200

    form validations

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -8,18 +8,43 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
 
-  public registerForm = this.fb.group({
+  public formSubmitted = false;
+
+  public registerForm: any = this.fb.group({
     nombre: ['Fernando', Validators.required ],
-    email: ['test100@gmail.com', Validators.required ],
-    password: ['123456', Validators.required ],
-    password2: ['123456', Validators.required ],
+    email: ['test100@gmail.com', [Validators.required, Validators.email] ],
+    password: ['', Validators.required ],
+    password2: ['', Validators.required ],
     terminos: [false, Validators.required ],
   });
 
   constructor( private fb: FormBuilder) {}
 
   crearUsuario() {
+    this.formSubmitted = true;
     console.log( this.registerForm.value );
+
+    if(this.registerForm.valid) {
+      console.log('posteando formulario')
+    } else {
+      console.log('Formulario no es correcto...');
+    }
   }
+
+  campoNoValido( campo: string ):boolean {
+
+    if( this.registerForm.get(campo).invalid && this.formSubmitted ) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
+  aceptaTerminos() {
+    return !this.registerForm.get('terminos').value && this.formSubmitted;
+  }
+
+
 
 }
