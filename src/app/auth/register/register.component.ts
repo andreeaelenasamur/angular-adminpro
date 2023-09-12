77ca1f2fa8a36894f216f565aa2da5b36d12f515
Andreea <andelenasamur@gmail.com>
2023-09-12 09:50:47 +0200

    reactive forms

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  public registerForm = this.fb.group({
+    nombre: ['Fernando', Validators.required ],
+    email: ['test100@gmail.com', Validators.required ],
+    password: ['123456', Validators.required ],
+    password2: ['123456', Validators.required ],
+    terminos: [false, Validators.required ],
+  });
+
+  constructor( private fb: FormBuilder) {}
+
+  crearUsuario() {
+    console.log( this.registerForm.value );
+  }
 
 }
