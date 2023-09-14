0e44401d99666b3a2b3248f4134ffbd7ceb43833
Andreea <andelenasamur@gmail.com>
2023-09-14 10:19:08 +0200

    show user profile image

--- src/app/models/usuario.model.ts
+++ src/app/models/usuario.model.ts
@@ -1,3 +1,6 @@
+import { environment } from "src/environments/environment"
+
+const base_url = environment.base_url;
 export class Usuario {
 
   constructor(
@@ -12,10 +15,20 @@ export class Usuario {
 
   }
 
-  imprimirUsuario() {
-    console.log(this.nombre);
-  }
+  get imagenUrl() {
 
+    if ( this.img?.includes('https') ) {
+      return this.img;
+    }
+
+
+    if ( this.img ) {
+      return `${base_url}/upload/usuarios/${this.img}`;
+    } else {
+      return `${base_url}/upload/usuarios/no-image`;
+
+    }
+  }
 
 
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -63,7 +63,6 @@ export class UsuarioService {
 
         this.usuario = new Usuario(nombre, email, '', img, google, role, uid);
 
-        this.usuario.imprimirUsuario();
         localStorage.setItem('token', resp.token);
       }),
       map( resp => true ),
