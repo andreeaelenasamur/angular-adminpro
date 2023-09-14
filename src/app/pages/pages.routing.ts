df2d040a742a0390415de40bec258ccea5b76d1a
Andreea <andelenasamur@gmail.com>
2023-09-14 16:25:26 +0200

    create the users component

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -11,6 +11,9 @@ import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
 import { PerfilComponent } from './perfil/perfil.component';
 
+// Mantenimientos
+import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
+
 const routes: Routes = [
   {
     path: 'dashboard',
@@ -24,6 +27,12 @@ const routes: Routes = [
       { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' } },
       { path: 'rxjs', component: RxjsComponent, data: { titulo: 'RxJs' } },
       { path: 'perfil', component: PerfilComponent, data: { titulo: 'Perfil de usuario' } },
+
+      // Mantenimientos
+      { path: 'usuarios', component: UsuariosComponent, data: { titulo: 'Usuario de aplicación' } },
+      // { path: 'hospitales', component: UsuariosComponent, data: { titulo: 'Usuario de aplicación' } },
+      // { path: 'medicos', component: UsuariosComponent, data: { titulo: 'Usuario de aplicación' } },
+
     ]
   },
 ];
