f47148f3f8a1f40d2fd18603f35201804f16edc2
Andreea <andelenasamur@gmail.com>
2023-09-14 10:31:59 +0200

    email optimizations

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -58,14 +58,14 @@ export class UsuarioService {
         'x-token': token
       }
     }).pipe(
-      tap( (resp: any) => {
-        const { email, google, nombre, role, img, uid } = resp.usuario;
+      map( (resp: any) => {
+        const { email, google, nombre, role, img = '', uid } = resp.usuario;
 
         this.usuario = new Usuario(nombre, email, '', img, google, role, uid);
 
         localStorage.setItem('token', resp.token);
+        return true;
       }),
-      map( resp => true ),
       catchError( error => of(false) )
     );
   }
