aa22e85709a5262c22bef68cc3ff0b364d36610d
Andreea <andelenasamur@gmail.com>
2023-09-20 09:34:35 +0200

    prepare the doctors creation form

--- src/app/pages/mantenimientos/medicos/medico.component.ts
+++ src/app/pages/mantenimientos/medicos/medico.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Hospital } from 'src/app/models/hospital.model';
+
+import { HospitalService } from 'src/app/services/hospital.service';
 
 @Component({
   selector: 'app-medico',
@@ -6,6 +10,36 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class MedicoComponent {
+export class MedicoComponent implements OnInit{
+
+  public medicoForm: FormGroup;
+  public hospitales: Hospital[] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private hospitalService: HospitalService
+    ) {}
+
+  ngOnInit(): void {
+    this.medicoForm = this.fb.group({
+      nombre: ['Hernando', Validators.required],
+      hospital: ['', Validators.required],
+    });
+
+    this.cargarHospitales();
+  }
+
+  cargarHospitales() {
+    this.hospitalService.cargarHospitales()
+      .subscribe( (hospitales: Hospital[]) => {
+        console.log(hospitales)
+        this.hospitales = hospitales;
+      })
+  }
+
+  guardarMedico() {
+    console.log(this.medicoForm.value)
+  }
+
 
 }
